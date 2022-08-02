c22c37f79be6ef82cde3330b834898ea9dece5fc
José FernandoTolentino Filho <zetolenttino@gmail.com>
2022-08-01 23:08:09 -0300

    limpando codigo

--- scripts.js
+++ scripts.js
@@ -10,31 +10,15 @@ function jogar(escolha) {
         // alert("EMPATE - JOGUE NOVAMENTE");
       } else if (jogador === 1 && computador === 2) {
         document.getElementById("visor").value = "COMPUTADOR VENCEU"
-        // alert("COMPUTADOR VENCEU");
       } else if (jogador === 1 && computador === 3) {
         document.getElementById("visor").value = "VOCÊ VENCEU"
-        // alert("VOCÊ VENCEU");
       } else if (jogador === 2 && computador === 1) {
         document.getElementById("visor").value = "VOCÊ VENCEU"
-        // alert("VOCÊ VENCEU");
       } else if (jogador === 2 && computador === 3) {
         document.getElementById("visor").value = "COMPUTADOR VENCEU"
-        // alert("COMPUTADOR VENCEU");
       } else if (jogador === 3 && computador === 1) {
         document.getElementById("visor").value = "COMPUTADOR VENCEU"
-        // alert("COMPUTADOR VENCEU");
       } else if (jogador === 3 && computador === 2) {
         document.getElementById("visor").value = "VOCÊ VENCEU"
-        // alert("VOCÊ VENCEU");
       }
 }
-
-// function visor() {
-//   let visor = document.querySelector("#visor");
-// }
-
-// addElemento()
-// calcVencedor("papel", "pedra");
-
-// primeiraJogada(3)
-// segundaJogada(2)
